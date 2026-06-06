a9e0c39517f27e8a4770d39fb225c61dd80414f0
Shaurya-Jha <shauryajha1602@gmail.com>
2026-06-06 13:23:46 +0530

    old name to new name

--- svelte.config.js
+++ svelte.config.js
@@ -25,7 +25,8 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/minimalist-personal-portfolio' : ''
+			// base: process.env.NODE_ENV === 'production' ? '/minimalist-personal-portfolio' : ''
+			base: process.env.NODE_ENV === 'production' ? '/dev' : ''
 		}
 	},
 	preprocess: [
